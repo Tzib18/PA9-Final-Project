5ae29ef8fc7bb7feabb37fa1929c76d23993461f
Austin Tzib <austin.tzib@wsu.edu>
2025-04-16 01:26:11 -0700

    Add project files.

---
+++ PA9 Final Project/main.cpp
@@ -0,0 +1,21 @@
+#include "SFML/Graphics.hpp"
+#include "SFML/System.hpp"
+#include "SFML/Audio.hpp"
+#include "SFML/Window.hpp"
+int main(void)
+{										// x, y 
+	sf::RenderWindow windows(sf::VideoMode(800, 800), "Test run of using sfml", sf::Style::Titlebar | sf::Style::Close); // this sets up the window screen 
+	sf::Event event; // event is when something is happening 
+	// like if they want to move right you will call event and a keybaord fucntion like A. 
+	while (windows.isOpen()) { // if the user compiles and the screen is open 
+		while (windows.pollEvent(event)) { // if their is a window open it will proccess this loop
+			// and render the window 
+			if (event.type == sf::Event::Closed) {// if you hit the X button this will call 
+				// to close the entire window itself. 
+				windows.close(); 
+			}
+		}
+	}
+
+	return 0; 
+}
