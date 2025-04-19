640af63f91a90a4f47e19f1cac4374fcdaf11548
Tzib18 <austin.tzib@wsu.edu>
2025-04-18 17:38:18 -0700

    tested out the purple glob character but it is to big for the screen

--- PA9 Final Project/main.cpp
+++ PA9 Final Project/main.cpp
@@ -4,184 +4,26 @@
 #include "SFML/Window.hpp"
 #include <iostream>
 #include "Player.hpp"
-#include "Platform.hpp"
 
 // end goal worked i was able to start it on the pc and then transfer it to my laptop
+// end goal worked i was able to start it on the pc and then transfer it to my laptop
+// end goal worked i was able to start it on the pc and then transfer it to my laptop
+// end goal worked i was able to start it on the pc and then transfer it to my laptop
 int main(void)
-{										
-/***** Window Setup *****/
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Doodle Jump - PA9");
-    window.setFramerateLimit(60);
-
-    /***** View (Camera) *****/
-    sf::View view = window.getDefaultView();
-
-    /***** Background Setup *****/
-    sf::Texture bgTexture;
-    if (!bgTexture.loadFromFile("paper_background.jpg"))
-    {
-        std::cerr << "Failed to load paper_background.jpg" << std::endl;
-        return -1;
-    }
-
-    sf::Vector2u bgSize = bgTexture.getSize();
-    float bgScaleX = static_cast<float>(window.getSize().x) / bgSize.x;
-    float bgScaleY = static_cast<float>(window.getSize().y) / bgSize.y;
-
-    std::vector<sf::Sprite> backgrounds;
-    for (int y = 0; y <= 1800; y += window.getSize().y)
-    {
-        sf::Sprite bg(bgTexture);
-        bg.setScale(bgScaleX, bgScaleY);
-        bg.setPosition(0.f, -static_cast<float>(y));
-        backgrounds.push_back(bg);
-    }
-
-    float highestBG = backgrounds.front().getPosition().y;
-
-    /***** Platform Setup *****/
-    sf::Texture platformTexture;
-    if (!platformTexture.loadFromFile("mock_platform.png"))
-    {
-        std::cerr << "Failed to load mock_platform.png" << std::endl;
-        return -1;
-    }
-
-    std::vector<Platform> platforms;
-    platforms.emplace_back(100.f, 500.f, &platformTexture);
-    platforms.emplace_back(300.f, 400.f, &platformTexture);
-    platforms.emplace_back(150.f, 300.f, &platformTexture);
-    platforms.emplace_back(350.f, 200.f, &platformTexture);
-    platforms.emplace_back(200.f, 100.f, &platformTexture);
-
-    float highestPlatformY = 100.f;
-
-    /***** Player Setup *****/
-    Player player;
-
-    /***** Time and Random Seed *****/
-    sf::Clock clock;
-    std::srand(static_cast<unsigned int>(std::time(nullptr)));
-
-    /***** Game Loop *****/
-    while (window.isOpen())
-    {
-        /***** Event Polling *****/
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        /***** Delta Time *****/
-        float deltaTime = clock.restart().asSeconds();
-
-        /***** Player Update *****/
-        player.handleInput();
-        player.update(deltaTime);
-
-        /***** Horizontal Wraparound with Height Locking *****/
-        sf::Vector2f playerPos = player.getPosition();
-        float playerWidth = player.getGlobalBounds().width;
-        float yLock = playerPos.y; // Save current height before teleport
-
-        if (playerPos.x > view.getCenter().x + 400.f)
-        {
-            player.setPosition(view.getCenter().x - 400.f - playerWidth, yLock);
-        }
-        if (playerPos.x + playerWidth < view.getCenter().x - 400.f)
-        {
-            player.setPosition(view.getCenter().x + 400.f, yLock);
-        }
-
-        /***** Platform Collision *****/
-        for (auto& platform : platforms)
-        {
-            if (player.getBounds().intersects(platform.getBounds()))
-            {
-                float playerBottom = player.getPosition().y + player.getGlobalBounds().height;
-                float platformTop = platform.getPosition().y;
-                if (player.getVelocityY() > 0 && playerBottom < platformTop + 10.f)
-                {
-                    player.jump();
-                }
-            }
-        }
-
-        /***** Camera Follows Player Upward *****/
-        if (player.getPosition().y < view.getCenter().y - 100.f)
-        {
-            view.setCenter(view.getCenter().x, player.getPosition().y + 100.f);
-            window.setView(view);
-        }
-
-        /***** Game Over: Fell Below Screen *****/
-        if (player.getPosition().y > view.getCenter().y + 300.f)
-        {
-            std::cout << "Game Over!" << std::endl;
-            window.close();
-        }
-
-        /***** Add Background Tiles Before Needed *****/
-        float topOfView = view.getCenter().y - window.getSize().y / 2.f;
-
-        while (topOfView - window.getSize().y < highestBG)
-        {
-            sf::Sprite newBG(bgTexture);
-            newBG.setScale(bgScaleX, bgScaleY);
-            highestBG -= window.getSize().y;
-            newBG.setPosition(0.f, highestBG);
-            backgrounds.push_back(newBG);
-        }
-
-        /***** Remove Backgrounds Far Below Screen *****/
-        backgrounds.erase(std::remove_if(backgrounds.begin(), backgrounds.end(), [&view, &window](const sf::Sprite& bg)
-            {
-                float bgTop = bg.getPosition().y;
-                float viewBottom = view.getCenter().y + window.getSize().y / 2.f;
-                return bgTop > viewBottom + window.getSize().y;
-            }),
-            backgrounds.end()
-        );
-
-        /***** Generate New Platforms Above View (Safer Spacing) *****/
-        while (highestPlatformY > view.getCenter().y - 300.f)
-        {
-            // Random vertical gap between 80 and 130 pixels
-            highestPlatformY -= static_cast<float>(rand() % 50 + 80);
-
-            // Keep platforms within 400px window centered on screen
-            float centerX = view.getCenter().x;
-            float x = static_cast<float>((rand() % 400) + (centerX - 200));
-
-            platforms.emplace_back(x, highestPlatformY, &platformTexture);
-        }
-
-        /***** Remove Platforms Far Below Screen *****/
-        platforms.erase(std::remove_if(platforms.begin(), platforms.end(), [&view](const Platform& p)
-            {
-                return p.getPosition().y > view.getCenter().y + 400.f;
-            }),
-            platforms.end()
-        );
-
-        /***** Drawing Everything *****/
-        window.clear();
-
-        for (auto& bg : backgrounds)
-        {
-            window.draw(bg);
-        }
-
-        for (auto& platform : platforms)
-        {
-            window.draw(platform);
-        }
-
-        window.draw(player);
-        window.display();
-    }
-
-    return 0;
+{										// x, y 
+	sf::RenderWindow windows(sf::VideoMode(800, 800), "Test run of using sfml", sf::Style::Titlebar | sf::Style::Close); // this sets up the window screen 
+	sf::Event event; // event is when something is happening 
+	// like if they want to move right you will call event and a keybaord fucntion like A. 
+	while (windows.isOpen()) { // if the user compiles and the screen is open 
+		while (windows.pollEvent(event)) { // if their is a window open it will proccess this loop
+			// and render the window 
+			if (event.type == sf::Event::Closed) {// if you hit the X button this will call 
+				// to close the entire window itself. 
+				windows.close(); 
+			}
+		}
+	}
+	// hello  
+	// welcome 
+	return 0; 
 }
