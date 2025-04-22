2e80160a7b13a182d8ae208773818d37afeb8962
Tzib18 <austin.tzib@wsu.edu>
2025-04-22 16:16:29 -0700

    made the player score cpp file

---
+++ PA9 Final Project/player score.cpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include "Player.hpp"
+#include "Platform.hpp"
+#include "Menu.hpp"
+#include "BackgroundAudioTrack.hpp"
+#include "Enemy.hpp"
+#include "Bullet.hpp"
+
+
+void PlayerScore(int& score, const std::vector<Enemy>& Enemies1, const std::vector<Enemy>& Enemies2) {
+
+
+}
